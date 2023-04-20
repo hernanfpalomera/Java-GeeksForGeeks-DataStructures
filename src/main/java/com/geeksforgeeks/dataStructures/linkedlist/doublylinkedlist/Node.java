d307deab144d9c6ca60ed0d9e8b782792762eab3
Hernan Fonseca Palomera <hernanfpalomera@gmail.com>
2023-04-20 10:26:54 -0600

    Adding Siemple Linked List excercises

---
+++ src/main/java/com/geeksforgeeks/dataStructures/linkedlist/doublylinkedlist/Node.java
@@ -0,0 +1,16 @@
+package com.geeksforgeeks.dataStructures.linkedlist.doublylinkedlist;
+
+public class Node {
+    int data;
+    Node prev;
+    Node next;
+
+    // Constructor to create a new node
+    // next and prev is by default initialized as null
+    Node(int d) {
+        data = d;
+        prev = null;
+        next = null;
+    }
+}
+
---
+++ src/main/java/com/geeksforgeeks/dataStructures/linkedlist/simplelinkedlist/Node.java
@@ -0,0 +1,11 @@
+package com.geeksforgeeks.dataStructures.linkedlist.simplelinkedlist;
+
+public class Node {
+
+        int data;
+        Node next;
+
+        // Constructor to create a new node
+        Node(int d) {data = d; next = null; }
+    }
+
